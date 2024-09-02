7096fe38606e215744f5f979392eb3ce9828b0c5
daizharyk <aizharyk76@gmail.com>
2024-09-02 14:52:20 +0500

    добавил стиль

--- index.js
+++ index.js
@@ -17,15 +17,32 @@ document.querySelectorAll('.nav-menu').forEach(menu => {
     });
   });
 
-  menu.addEventListener('mouseout', () => {
+
+menu.addEventListener('mouseout', () => {
     const dropdownClass = `${menu.classList[1]}-dropdown`; 
     const dropdown = document.querySelector(`.${dropdownClass}`);
     dropdown.style.display = 'none';
   });
 });
 
-document.getElementById('filterSortToggle').addEventListener('click',function(){
+
+
+document.getElementById('filterSortToggle').addEventListener('click', function () {
   const sidebar = document.getElementById('sidebar');
-  
-  sidebar.classList.toggle('sidebar');
+  const filterDropdown = document.querySelector('.product-filter_dropdown');
+
+  sidebar.style.display = sidebar.style.display === 'none' || sidebar.style.display === '' ? 'block' : 'none';
+  filterDropdown.classList.toggle('active'); 
 })
+
+document.querySelectorAll('.nav-list-item').forEach(item => {
+  item.addEventListener('click', () => {
+    document.querySelectorAll('.nav-list-item.active').forEach(activeItem => {
+      if (activeItem !== item) {
+        activeItem.classList.remove('active');
+      }
+    });
+    item.classList.toggle('active');
+  });
+});
+
