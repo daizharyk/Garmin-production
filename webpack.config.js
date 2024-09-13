9ea865b919552308d319b49185712cbe8f1487b8
daizharyk <aizharyk76@gmail.com>
2024-09-13 12:20:47 +0500

    1

--- webpack.config.js
+++ webpack.config.js
@@ -1,90 +1,82 @@
 
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  optimization:{
-    minimize: true ,
+  mode: 'development', // Измените на 'production' для финальной сборки
+  entry: {
+    main: path.resolve(__dirname, 'src/js/index.js'),
+    animation: path.resolve(__dirname, 'src/js/animation.js'),
+    html: path.resolve(__dirname, 'src/js/html.js'),
+    itempage: path.resolve(__dirname, 'src/js/itempage.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+    assetModuleFilename: 'img/[name][ext]',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3001,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|webp)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name][ext]', // Обновлено для правильного расположения изображений
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Garmin',
+      filename: 'index.html',
+      template: 'src/index.html',
+      chunks: ['animation', 'main'],
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Product Page',
+      filename: 'pages/itempage.html',
+      template: 'src/pages/itempage.html',
+      chunks: ['itempage', 'animation'],
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Shipping',
+      filename: 'pages/shipping.html',
+      template: 'src/pages/shipping.html',
+      chunks: ['animation'],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/img', to: 'img' },
+        { from: 'src/data/item.json', to: 'data/item.json' },
+      ],
+    }),
+  ],
+  optimization: {
+    minimize: true,
     minimizer: [
       new TerserPlugin(),
       new CssMinimizerPlugin(),
     ],
   },
-mode: "development", 
-  entry: {
-  main: path.resolve(__dirname, 'src/js/index.js'),
-  animation: path.resolve(__dirname, 'src/js/animation.js'),
-  html: path.resolve(__dirname, 'src/js/html.js'),
-  itempage: path.resolve(__dirname, 'src/js/itempage.js'),
-  },
-output:{
-  path: path.resolve(__dirname, "dist"),
-  filename: "[name][contenthash].js",
-  clean: true,
-  assetModuleFilename: "[name][ext]",
-} ,
-devServer:{
-  static: {
-    directory: path.resolve(__dirname, 'dist')
-  },
-    port: 3001,
-    open: true,
-    hot: true ,
-  
-},
-module: {
-  rules: [
-    
-    {
-      test: /\.(png|svg|jpg|jpeg|webp)$/i,
-      type: 'asset/resource',
-      generator: {
-        filename: 'img/[name][ext][query]', // Все изображения будут перемещаться в dist/img
-      },
-  },
-  {
-    test: /\.css$/i,
-    use: ['style-loader', 'css-loader'],
-  },
-  
-],
-},
-
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-    }),
-  new HtmlWebpackPlugin({
-  title: "Garmin",
-  filename: "index.html",
-  template: "src/index.html",
-  chunks: ["animation" , "main"],
-  }),
-  new HtmlWebpackPlugin({
-  title: "Product Page",
-  filename: "pages/itempage.html",
-  template: "src/pages/itempage.html",
-  chunks: ["itempage" , "animation"],
-  }),
-  new HtmlWebpackPlugin({
-  title: "Shipping",
-  filename: "pages/shipping.html",
-  template: "src/pages/shipping.html",
-  chunks: ["animation"],
-  }),
-  new CopyWebpackPlugin({
-    patterns: [
-      { from: 'src/img', to: 'img' }, 
-    ],
-  }),
-  new CopyWebpackPlugin({
-    patterns: [
-      { from: 'src/data/item.json', to: 'data/item.json' }, 
-    ],
-  }),
-],
 };
