0e03352446951e3e6b3d074c326db3ae0828daed
daizharyk <aizharyk76@gmail.com>
2024-10-08 11:34:20 +0500

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,21 @@
-import {fetchData} from "./itemService.js"
+import { fetchData } from "./itemService.js";
 import "../style/style.css";
 import "../style/shipping.css";
 import "../style/itempage.css";
 
-
 async function loadModule() {
   try {
-    const data = await fetchData(); 
-
-    const module = await import(/* webpackChunkName: "html-module" */ "./htmlBuilder.js");
+    const data = await fetchData();
+    if (!data || !Array.isArray(data)) {
+      throw new Error("Данные не загружены или не в правильном формате");
+    }
+    const module = await import(
+      /* webpackChunkName: "html-module" */ "./htmlBuilder.js"
+    );
     const { createCards, scrollToItems, createNewCards } = module;
-
-    createCards(data); 
-    scrollToItems();
     createNewCards();
+    createCards(data);
+    scrollToItems();
   } catch (error) {
     console.error("Ошибка при загрузке модуля:", error);
   }
