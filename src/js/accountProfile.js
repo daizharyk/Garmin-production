a34d9c6b881e66f2e7b666715a006939ae93bdf1
Didar <aizharyk78@gmail.com>
2024-12-12 23:15:55 +0500

    Добавил новые фича для формы редактирвания

--- src/js/accountProfile.js
+++ src/js/accountProfile.js
@@ -15,13 +15,41 @@ document.addEventListener("DOMContentLoaded", () => {
   const shippingForm = document.querySelector(".shipping-form-address");
   const editBtn = document.querySelector(".address-edit-btn");
   const infoCard = document.querySelector(".info-card");
-  const cancelBtn = document.querySelector(".cancel-btn");
+  const addressFormCancel = document.querySelector(".address-form-cancel");
   const numberEditBtn = document.querySelector(".number-edt-btn");
   const phoneNumberForm = document.querySelector(".phone-number-form");
   const phoneNumberContainer = document.querySelector(
     ".phone-number-container"
   );
   const phoneEditCancel = document.querySelector(".phone-number-cancel");
+  const accountEditBtn = document.querySelector(".account-edit-btn");
+  const accountDetailForm = document.querySelector(".account-detail-form");
+  const accountDetailWrapper = document.querySelector(
+    ".account-detail-wrapper"
+  );
+  const accountEditCancel = document.querySelector(".account-edit-cancel");
+  const updatePasswordForm = document.querySelector(".update-password-form");
+  const updatePasswordBtn = document.querySelector(".update-password-btn");
+  const passwordEditCancel = document.querySelector(".password-edit-cancel");
+
+  updatePasswordBtn.addEventListener("click", function () {
+    updatePasswordForm.classList.toggle("visible");
+    accountDetailWrapper.classList.add("hidden");
+  });
+  passwordEditCancel.addEventListener("click", function () {
+    updatePasswordForm.classList.remove("visible");
+    accountDetailWrapper.classList.remove("hidden");
+  });
+
+  accountEditBtn.addEventListener("click", function () {
+    accountDetailForm.classList.toggle("visible");
+    accountDetailWrapper.classList.add("hidden");
+  });
+  accountEditCancel.addEventListener("click", function () {
+    accountDetailForm.classList.remove("visible");
+    accountDetailWrapper.classList.remove("hidden");
+  });
+
   numberEditBtn.addEventListener("click", function () {
     phoneNumberForm.classList.toggle("visible");
     phoneNumberContainer.classList.add("hidden");
@@ -35,7 +63,8 @@ document.addEventListener("DOMContentLoaded", () => {
     infoCard.classList.add("hidden");
   });
 
-  cancelBtn.addEventListener("click", function () {
+  addressFormCancel.addEventListener("click", function () {
+    console.log("Cancel button clicked!");
     shippingForm.classList.remove("visible");
     infoCard.classList.remove("hidden");
   });
