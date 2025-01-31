1dca697881695bb01b23d76522138e15d290f5d4
Didar <aizharyk78@gmail.com>
2025-01-31 10:40:54 +0500

    1

--- src/js/searchingInput.js
+++ src/js/searchingInput.js
@@ -1,4 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const burgerButton = document.getElementById("burgerButton");
+  const navMenu = document.getElementById("navMenu");
+
+  if (burgerButton && navMenu) {
+    burgerButton.addEventListener("click", function () {
+      this.classList.toggle("active");
+      navMenu.style.display =
+        navMenu.style.display === "block" ? "none" : "block";
+    });
+    function handleResize4() {
+      const windowWidth = window.innerWidth;
+
+      if (windowWidth >= 1100) {
+        if (burgerButton && navMenu) {
+          burgerButton.classList.remove("active");
+          navMenu.style.display = "";
+        }
+      }
+    }
+    window.addEventListener("resize", handleResize4);
+    handleResize4();
+  }
+
   const searchInputMobile = document.getElementById("searchInputMobile");
 
   if (searchInputMobile) {
